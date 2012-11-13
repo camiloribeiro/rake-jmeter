c81dbb1588f84775d27d6e39bbab8404344df7e1
Camilo Ribeiro <camilo@camiloribeiro.com>
2012-11-13 16:38:40 -0200

    updating envs

--- config/env_config.rb
+++ config/env_config.rb
@@ -3,21 +3,9 @@ class Server < Struct.new(:name,:host,:port,:user); end
   @project_dir = "sample"
 
   STRESS_SERVERS_INTERNAL = [
-    Server.new('stress01','123.123.123.111', '8080', 'team'),
-    Server.new('stress02','123.123.123.112', '8080', 'team'),
-    Server.new('stress03','123.123.123.113', '8080', 'team'),
-    Server.new('stress04','123.123.123.114', '8080', 'team')
+    Server.new('vagrant01','192.168.1.11', '8080', 'vagrant'),
+    Server.new('vagrant02','192.168.1.11', '8080', 'vagrant'),
   ]
-
-  if ENV['MODE'] == 'INTERNAL'
-    STRESS_SERVERS_EXTERNAL = STRESS_SERVERS_INTERNAL
-  else
-    STRESS_SERVERS_EXTERNAL = [
-      Server.new('stress01','200.123.123.111', '8080', 'team'),
-      Server.new('stress02','200.123.123.112', '8080', 'team'),
-      Server.new('stress03','200.123.123.113', '8080', 'team'),
-      Server.new('stress04','200.123.123.114', '8080', 'team')
-    ]
-  end
+  STRESS_SERVERS_EXTERNAL = STRESS_SERVERS_INTERNAL
 
   MASTER = STRESS_SERVERS_INTERNAL[0]
