84bb08aae3adcf71d1192743de2e2ec25e1927fe
Camilo Ribeiro <camilo@camiloribeiro.com>
2012-11-13 17:03:00 -0200

    add port 22 to vagrant config

--- config/env_config.rb
+++ config/env_config.rb
@@ -3,8 +3,8 @@ class Server < Struct.new(:name,:host,:port,:user); end
   @project_dir = "sample"
 
   STRESS_SERVERS_INTERNAL = [
-    Server.new('vagrant01','192.168.1.11', '8080', 'vagrant'),
-    Server.new('vagrant02','192.168.1.11', '8080', 'vagrant'),
+    Server.new('vagrant01','192.168.1.11', '22', 'vagrant'),
+    Server.new('vagrant02','192.168.1.11', '22', 'vagrant'),
   ]
   STRESS_SERVERS_EXTERNAL = STRESS_SERVERS_INTERNAL
 
