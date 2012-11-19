07e5b26a9d29bdfc45d27fc10bc69d46eaad8b1a
Camilo Ribeiro <camilo@camiloribeiro.com>
2012-11-19 19:05:31 -0200

    add bootstrap with initial tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 rvm install 1.9.3
 rvm use 1.9.3
 bundle install
-vagrant box add lucid32 http://files.vagrantup.com/lucid32.box
+vagrant box add quantal64 https://github.com/downloads/roderik/VagrantQuantal64Box/quantal64.box
+vagrant up
+rake ssh:add_keys       
+rake sync:run
+rake ssh:install_java  
+rake ssh:setup_master
+rake ssh:internal_network_config
+rake ssh:internal_network_hosts
+
