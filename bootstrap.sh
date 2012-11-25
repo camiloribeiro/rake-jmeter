af78c41d3b89c2d02f16107ef4de6522b9b4ac97
Camilo Ribeiro <camilo@camiloribeiro.com>
2012-11-24 22:21:29 -0200

    add new structure to bootstrap and test in the end of it

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,12 @@ rvm install 1.9.3
 rvm use 1.9.3
 bundle install
 vagrant box add quantal64 https://github.com/downloads/roderik/VagrantQuantal64Box/quantal64.box
-vagrant up
+vagrant destroy vagrant01 -f
+vagrant destroy vagrant02 -f
+vagrant up vagrant01
+vagrant up vagrant02
 rake ssh:add_keys       
-rake sync:run
-rake ssh:install_java  
+rake ssh:install_java
 rake ssh:setup_master
-rake ssh:internal_network_config
-rake ssh:internal_network_hosts
 rake perf:sample:nominal
+rake test:all
