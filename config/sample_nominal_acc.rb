ad8a3fcc2187d5375ef8e031f9d6cbe414930792
Camilo Ribeiro <cribeiro@cribeiro.(none)>
2012-12-01 13:55:02 -0200

    add new acceptance criteria

--- config/sample_nominal_acc.rb
+++ config/sample_nominal_acc.rb
@@ -1,41 +1,40 @@
-
 #response time
-@min_response_time_under_500ms = 10
-@min_response_time_under_2s = 60
-@min_response_time_under_4s = 90
-@min_response_time_under_6s = 90
-@min_response_time_under_8s = 90
+@min_response_time_under_500ms = 30
+@min_response_time_under_2s = 90
+@min_response_time_under_4s = 95
+@min_response_time_under_6s = 97
+@min_response_time_under_8s = 99
 
 #max over 8s
-@max_over_8 = 0
+@max_over_8 = 1
 
 #max Standard Deviation
-@max_standard_deviation = 2000
+@max_standard_deviation = 500
 
 #max Standard Deviation
 @max_percentil_deviation = 10
 
 #Error Rate
-@max_error_rate = 2
+@max_error_rate = 1
 
 #Response time
-@max_avg_time = 4000
+@max_avg_time = 1000
 
 #max min time
-@max_min_time = 1000
+@max_min_time = 400
 
 #max maximum response time for a single request
-@max_max_time = 200000
+@max_max_time = 2100
 
 #MIN throughput
-@max_throughput = 0.3
+@max_throughput = 5
 
 #MIN samplers
-@min_samplers = 400
+@min_samplers = 150
 
 #max 90$
 @max_90 = 2000
 
 #max median
-@max_median = 2000
+@max_median = 1000
 
