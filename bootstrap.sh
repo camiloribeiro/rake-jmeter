28ce4f587cde5ffd15f5361c92045fb1922ef111
Camilo Ribeiro <camilo.ribeiro@klarna.com>
2014-02-28 18:07:00 +0100

    Adding variables for hostname and port to sample.jmx example

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
+-bundle install
+rake sync:install
+vagrant box add quantal64 https://github.com/downloads/roderik/VagrantQuantal64Box/quantal64.box
+vagrant destroy vagrant01 -f
+vagrant destroy vagrant02 -f
+vagrant up 
 rake ssh:add_keys       
 rake ssh:setup_master
 rake test:all
